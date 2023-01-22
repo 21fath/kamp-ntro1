90477bd433de1dbe63597e9f76633f6a8a442041
FATİH <FATH@LAPTOP-A68MU80D>
2023-01-22 13:16:55 +0300

    Add project files.

---
+++ kampıntro1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace kampıntro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //type safety - tip güvenliği
+            // do not repeat yourself - kendini tekrarlama
+            //Değer tutucu
+
+            string kategoriEtiketi = "kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGrisiYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi Butonu");
+            }
+
+            if (sistemeGrisiYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+
+
+
+        }
+    }
+}
